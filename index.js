7ef3e6a1309c7ca4da34a04d9a22c9677d3d76b4
vshvydun <vshvydun@gmail.com>
2026-04-01 00:34:18 +0300

    index.js

--- index.js
+++ index.js
@@ -176,3 +176,14 @@ function registerWebhook() {
   Logger.log('URL: ' + url);
   Logger.log(r.getContentText());
 }
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Бот Speak-Up працює! 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Веб-сервер запущено на порту ${PORT}`);
+});
